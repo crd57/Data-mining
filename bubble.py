8e6612caa45de582adacfe43aca52a622f84e10f
crd <chenrd57@gmail.com>
2018-04-10 15:27:42 +0800

    The second chapter exercises

---
+++ bubble.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 18-4-10 下午3:21
+# @Author  : Crd
+# @Email   : crd57@outlook.com
+# @File    : bublle.py
+# @Software: PyCharm
+"""
---
+++ dict.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 18-4-10 下午3:22
+# @Author  : Crd
+# @Email   : crd57@outlook.com
+# @File    : dict.py
+# @Software: PyCharm
+"""
