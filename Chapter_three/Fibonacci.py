270015f23dccbd0b3dc3597d5aa33295c7d52103
crd <chenrd57@gmail.com>
2018-04-11 20:10:52 +0800

    The third chapter exercises

---
+++ Chapter_three/Fibonacci.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 18-4-10 下午6:47
+# @Author  : Crd
+# @Email   : crd57@outlook.com
+# @File    : Fibonacci.py
+# @Software: PyCharm
+"""
+
+c = lambda x: int(1/(5**(1/2))*(((1+5**(1/2))/2)**x - ((1-5**(1/2))/2)**x))
+
+print(c(10))
